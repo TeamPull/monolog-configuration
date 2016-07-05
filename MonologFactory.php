515ee081845518f2c7801d91760e7648a57a2ac2
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2016-07-05 18:44:37 +0200

    Update MonologFactory.php

--- MonologFactory.php
+++ MonologFactory.php
@@ -1,37 +1,22 @@
 <?php
-/**
-* This file is part of OXID eShop Community Edition.
-*
-* OXID eShop Community Edition is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* OXID eShop Community Edition is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @link      http://www.oxid-esales.com
-* @copyright (C) OXID eSales AG 2003-2016
-* @version   OXID eShop CE
-*/
-namespace OxidEsales\Eshop\Core;
+namespace Monolog\Configuration;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\ErrorHandler;
 use Monolog\Processor\PsrLogMessageProcessor;
-use OxidEsales\Eshop\Core\Contract\LoggerFactoryInterface;
 use Symfony\Component\Yaml\Yaml;
+
 /**
  * Class LoggerFactory
- * @package OxidEsales\Eshop\Core
  */
 class MonologFactory implements LoggerFactoryInterface
 {
+    protected $vars;
+
+    function __construct($vars){
+
+    }
+
     protected $monologConfig;
     protected function loadMonologConfig()
     {
