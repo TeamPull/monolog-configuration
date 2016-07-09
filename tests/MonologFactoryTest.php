77d711a41fe56bc1be382a33bcced8c45880af50
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2016-07-09 22:22:50 +0200

    Update MonologFactoryTest.php

--- tests/MonologFactoryTest.php
+++ tests/MonologFactoryTest.php
@@ -5,6 +5,10 @@ class MonologFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetLogger()
     {
+        $vars = [
+            'kernel.root_dir'=>'.',
+            'kernel.logs_dir'=>'.'
+        ];
         $lf = new MonologFactory($vars);
         $log = $lf->getLogger();
         $this->assertInstanceOf('Psr\Logger',$log);
